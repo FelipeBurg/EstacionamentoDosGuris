20f182af6f9666b1cc534e2466d2161cecb5f5a0
Felipe Burgdurf <96593150+FelipeBurg@users.noreply.github.com>
2024-10-08 14:08:18 +0000

    "Arrumado o cálculo de Pernoite e algumas alterações"

--- src/Service/CalculoTarifa.java
+++ src/Service/CalculoTarifa.java
@@ -5,43 +5,44 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class CalculoTarifa {
+    private double tarifaPorHora = 5.90;
+    private double tarifaAdicional = 2.50;
+    private double tarifaPernoite = 50.00;
+    private int tempoCortesia = 15;
+    private double descontoVip = 0.5;
 
     // Método para calcular o valor do ticket
     public double calcularValor(Duration duracao, LocalDateTime horaEntrada, LocalDateTime horaSaida, boolean isVIP) {
         long minutos = duracao.toMinutes();
         double valor;
-        LocalDateTime horaEntradaSaida = horaEntrada;
-        LocalDateTime horaSaidaSaida = horaSaida;
-        if (isOutroDia(horaEntradaSaida, horaSaida)) {
-            valor = 50.00; // Valor fixo do pernoite
+        if (pernoitou(horaEntrada, horaSaida)) {
+            valor = tarifaPernoite * numDiasPernoite(horaEntrada, horaSaida); // Valor fixo do pernoite
         } else {
             // Caso não seja pernoite, calcula valor normalmente
-            if (minutos <= 15) {
+            if (minutos <= tempoCortesia) {
                 valor = 0.0;  // 15 minutos de cortesia
             } else if (minutos <= 60) {
-                valor = 5.90;  // Até 1 hora
+                valor = tarifaPorHora;  // Até 1 hora
             } else {
                 long horas = duracao.toHours();
-                valor = 5.90 + (horas - 1) * 2.50;  // Após 1 hora, R$ 2,50 por hora adicional
+                valor = tarifaPorHora + (horas - 1) * tarifaAdicional;  // Após 1 hora, R$ 2,50 por hora adicional
             }
         }
 
         // Aplica desconto VIP se aplicável
         if (isVIP) {
-            valor *= 0.5;  // Desconto VIP de 50%
+            valor *= descontoVip;  // Desconto VIP de 50%
         }
 
         return valor;
     }
 
-    public boolean isOutroDia(LocalDateTime horaEntrada, LocalDateTime horaSaida) {
-        // Considera-se pernoite se a entrada for antes das 2h e a saída for após as 8h do dia seguinte
-        LocalDateTime limiteSaida = horaEntrada.plusDays(1).withHour(8).withMinute(0).withSecond(0);
-        if (horaEntrada.getHour() < 2 && horaSaida.isAfter(limiteSaida)){
-            return true;
-        }else {
-            return false;
-        }
+    public boolean pernoitou(LocalDateTime entrada, LocalDateTime saida) {
+        return saida.toLocalTime().isAfter(LocalTime.of(8, 0)) && saida.toLocalDate().isAfter(entrada.toLocalDate());
+    }
+
+    public long numDiasPernoite(LocalDateTime entrada, LocalDateTime saida) {
+        return saida.toLocalDate().toEpochDay() - entrada.toLocalDate().toEpochDay();
     }
 
 }
